f2b9c47bdd59d65edaa8e8a4ff97cfec26be6426
YaoWeiXin <419576308@qq.com>
2020-11-17 15:00:55 +0800

    redis操作类

--- redis/Redis.go
+++ redis/Redis.go
@@ -1,20 +1,20 @@
-package linking_go_agile
+package redis
 
 type Redis struct {
 }
 
-func Init(redisConfig RedisConfig) {
+func Init(redisConfig RConfig) {
 	initRedis(redisConfig)
 }
 
-func RedisDel(db string, key string) {
+func RDel(db string, key string) {
 	//c.radius 即为 Circle 类型对象中的属性
 	conn := getConn(db)
 	conn.Do("del", key)
 	defer releaseConn(conn)
 }
 
-func RedisExists(db string, key string) bool {
+func RExists(db string, key string) bool {
 	//c.radius 即为 Circle 类型对象中的属性
 	conn := getConn(db)
 	rev, _ := conn.Do("exists", key)
@@ -22,38 +22,38 @@ func RedisExists(db string, key string) bool {
 	return string(rev.([]byte)) == "1"
 }
 
-func RedisExpire(db string, key string, time int) {
+func RExpire(db string, key string, time int) {
 	//c.radius 即为 Circle 类型对象中的属性
 	conn := getConn(db)
 	conn.Do("exists", key, time)
 	defer releaseConn(conn)
 }
 
-func RedisGet(db string, key string) string {
+func RGet(db string, key string) string {
 	conn := getConn(db)
 	rev, _ := conn.Do("get", key)
 	defer releaseConn(conn)
 	return string(rev.([]byte))
 }
 
-func RedisSet(db string, key string, value string) {
+func RSet(db string, key string, value string) {
 	conn := getConn(db)
 	conn.Do("set", key, value)
 	defer releaseConn(conn)
 }
 
-func RedisSetNX(db string, key string, value string) bool {
+func RSetNX(db string, key string, value string) bool {
 	conn := getConn(db)
 	rev, _ := conn.Do("SETNX", key, value)
 	defer releaseConn(conn)
 	return string(rev.([]byte)) == "1"
 }
 
-func RedisIncr(db string, key string) {
-	RedisIncrBy(db, key, 1)
+func RIncr(db string, key string) {
+	RIncrBy(db, key, 1)
 }
 
-func RedisIncrBy(db string, key string, num int) {
+func RIncrBy(db string, key string, num int) {
 	conn := getConn(db)
 	conn.Do("incrby", key, num)
 	defer releaseConn(conn)
