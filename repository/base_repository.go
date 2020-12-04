3a7376396b4d4992de0c52a9ab44a25b47e39a61
YaoWeiXin <419576308@qq.com>
2020-12-04 16:28:58 +0800

    优化错误类型

--- repository/base_repository.go
+++ repository/base_repository.go
@@ -8,6 +8,7 @@ package repository
 
 import (
 	"github.com/ganeryao/linking-go-agile/common"
+	"github.com/ganeryao/linking-go-agile/errors"
 	"github.com/ganeryao/linking-go-agile/mysql"
 	"github.com/ganeryao/linking-go-agile/redis"
 )
@@ -15,14 +16,21 @@ import (
 type BaseRepository struct {
 }
 
-func GetAlways(db string, table string, dest interface{}, sql string, id int64) interface{} {
+func GetAlways(db string, table string, dest interface{}, sql string, id int64) (interface{}, *errors.Error) {
 	var key = "cache:" + table + ":" + common.Int64ToStr(id)
 	var str = redis.RGet(db, key)
 	if common.IsEmpty(str) {
 		mysql.MFindOne(db, dest, sql, id)
-		redis.RSet(db, key, common.ConvertJson(dest))
+		str, err := common.ConvertJson(dest)
+		if err != nil {
+			return nil, err
+		}
+		redis.RSet(db, key, str)
 	} else {
-		common.ParseJson(str, dest)
+		_, err := common.ParseJson(str, dest)
+		if err != nil {
+			return nil, err
+		}
 	}
-	return dest
+	return dest, nil
 }
