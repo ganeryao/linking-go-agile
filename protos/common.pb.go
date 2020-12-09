37156abad756d4f1bb4ac7603cbb838a55f7d017
YaoWeiXin <419576308@qq.com>
2020-12-09 14:20:10 +0800

    增加消息协议

--- protos/common.pb.go
+++ protos/common.pb.go
@@ -152,6 +152,61 @@ func (x *LResult) GetData() string {
 	return ""
 }
 
+type LMsg struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Api string `protobuf:"bytes,1,opt,name=api,proto3" json:"api,omitempty"`
+	Msg string `protobuf:"bytes,2,opt,name=msg,proto3" json:"msg,omitempty"`
+}
+
+func (x *LMsg) Reset() {
+	*x = LMsg{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protos_common_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *LMsg) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*LMsg) ProtoMessage() {}
+
+func (x *LMsg) ProtoReflect() protoreflect.Message {
+	mi := &file_protos_common_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use LMsg.ProtoReflect.Descriptor instead.
+func (*LMsg) Descriptor() ([]byte, []int) {
+	return file_protos_common_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *LMsg) GetApi() string {
+	if x != nil {
+		return x.Api
+	}
+	return ""
+}
+
+func (x *LMsg) GetMsg() string {
+	if x != nil {
+		return x.Msg
+	}
+	return ""
+}
+
 var File_protos_common_proto protoreflect.FileDescriptor
 
 var file_protos_common_proto_rawDesc = []byte{
@@ -165,8 +220,11 @@ var file_protos_common_proto_rawDesc = []byte{
 	0x63, 0x6f, 0x64, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x63, 0x6f, 0x64, 0x65,
 	0x12, 0x10, 0x0a, 0x03, 0x6d, 0x73, 0x67, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x6d,
 	0x73, 0x67, 0x12, 0x12, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09,
-	0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x42, 0x09, 0x5a, 0x07, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x22, 0x2a, 0x0a, 0x04, 0x4c, 0x4d, 0x73, 0x67, 0x12, 0x10,
+	0x0a, 0x03, 0x61, 0x70, 0x69, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x61, 0x70, 0x69,
+	0x12, 0x10, 0x0a, 0x03, 0x6d, 0x73, 0x67, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x6d,
+	0x73, 0x67, 0x42, 0x09, 0x5a, 0x07, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -181,10 +239,11 @@ func file_protos_common_proto_rawDescGZIP() []byte {
 	return file_protos_common_proto_rawDescData
 }
 
-var file_protos_common_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_protos_common_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_protos_common_proto_goTypes = []interface{}{
 	(*LRequest)(nil), // 0: common.LRequest
 	(*LResult)(nil),  // 1: common.LResult
+	(*LMsg)(nil),     // 2: common.LMsg
 }
 var file_protos_common_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -224,6 +283,18 @@ func file_protos_common_proto_init() {
 				return nil
 			}
 		}
+		file_protos_common_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*LMsg); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -231,7 +302,7 @@ func file_protos_common_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_protos_common_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
