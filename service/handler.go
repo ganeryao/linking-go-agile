8b728f7ac2cbc585d45ff3aaab9f2bbae5714a7e
YaoWeiXin <419576308@qq.com>
2020-12-11 17:25:59 +0800

    删除http相关的类

--- service/handler.go
+++
@@ -1,8 +0,0 @@
-package service
-
-import "github.com/ganeryao/linking-go-agile/protos"
-
-type Handler interface {
-	TestFunc(req *protos.LRequest) *protos.LResult
-	DoFunc(req *protos.LRequest) *protos.LResult
-}
